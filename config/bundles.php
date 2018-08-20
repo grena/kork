f4efadf9767f2908577e325a8cb92308f0e4726c
Adrien Pétremann <hello@grena.fr>
2018-08-20 19:05:17 +0200

    Install hwi oauth connect bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
