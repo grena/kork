e6f8a6fda0b68d35a7c9ddb84fdf052bea7d69eb
Adrien Pétremann <hello@grena.fr>
2018-10-13 23:57:32 +0200

    User custom dumb controller

---
+++ src/Controller/LobbyController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class LobbyController extends Controller
+{
+    public function index()
+    {
+        return $this->render('login.html.twig');
+    }
+}
