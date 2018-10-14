e918837dfcd2a3e1100219291d65e41e66cc8b30
Adrien Pétremann <hello@grena.fr>
2018-10-14 17:16:11 +0200

    Handle logout and clean lobby, index & login page

--- src/Controller/LobbyController.php
+++ src/Controller/LobbyController.php
@@ -11,6 +11,20 @@ class LobbyController extends Controller
 {
     public function index()
     {
+        if (null === $this->getUser()) {
+            return $this->redirectToRoute('loggy');
+        }
+
+        return $this->redirectToRoute('lobby');
+    }
+
+    public function lobby()
+    {
+        return $this->render('lobby.html.twig', ['user' => $this->getUser()]);
+    }
+
+    public function login()
+    {
         return $this->render('login.html.twig');
     }
 }
